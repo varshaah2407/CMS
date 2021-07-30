9c1868837c716157a58a1c3dbbccde261384c4db
varshaah2407 <79756939+varshaah2407@users.noreply.github.com>
2021-07-30 14:07:56 +0530

    Add files via upload

---
+++ CMS Chatroom/build/precache-manifest.ed66e762e377d344b48c7ef5e9d403ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74e6bcb2cce57cb01d0b6c6a15e4e7a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98862ba282d4b095a5cc",
+    "url": "/static/css/2.ad76a385.chunk.css"
+  },
+  {
+    "revision": "efae771376b892461a6f",
+    "url": "/static/css/main.50d11b7e.chunk.css"
+  },
+  {
+    "revision": "98862ba282d4b095a5cc",
+    "url": "/static/js/2.62f028ca.chunk.js"
+  },
+  {
+    "revision": "2182b49f32ec797f3b9c375cbe5c7fb3",
+    "url": "/static/js/2.62f028ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efae771376b892461a6f",
+    "url": "/static/js/main.9bdb6075.chunk.js"
+  },
+  {
+    "revision": "45d009624ee73ac83b54",
+    "url": "/static/js/runtime-main.cdd681a7.js"
+  }
+]);
